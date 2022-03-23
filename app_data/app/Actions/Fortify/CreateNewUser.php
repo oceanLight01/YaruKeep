8c94eb1dde1bba255aaa3b44ab727fcf46d89535
oceanlight01 <oceanlight28@gmail.com>
2022-03-24 00:13:32 +0900

    add: サーバ側のパスワードバリデーションを追加変更

--- app_data/app/Actions/Fortify/CreateNewUser.php
+++ app_data/app/Actions/Fortify/CreateNewUser.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Password;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
 
 class CreateNewUser implements CreatesNewUsers
