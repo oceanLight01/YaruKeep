4c090d7906fb782b02253f94aa44a34a880f4fb0
oceanlight01 <oceanlight28@gmail.com>
2022-05-02 19:03:49 +0900

    add: フッターのスタイル付け

--- app_data/resources/ts/Components/Footer.tsx
+++ app_data/resources/ts/Components/Footer.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import styles from './../../scss/Footer.modules.scss';
+
 const Footer = () => {
     return (
-        <footer>
-            <p>yarukeep. 2022</p>
+        <footer className={styles.footer}>
+            <p className={styles.footer_copyright}>yarukeep. 2022</p>
         </footer>
     );
 };
